97911edecec5de24a0fba83dfc2db9698a9f1707
XingShiiDev <declanmesser1@outlook.com>
2020-10-22 18:36:55 +0100

    Initial Commit

---
+++ Akamai Version Checker/Program.cs
@@ -0,0 +1,49 @@
+﻿using Leaf.xNet;
+using System;
+using System.Threading;
+namespace Akamai_Version_Checker
+{
+    class Program
+    {
+        const string currentVersion = "1.64";
+        const string discordWebhook = "https://discordapp.com/api/webhooks/764163556173545492/VSDqWrXsfKIMNF-sn0PIvzrI_iGyg31cFCFnR_mEvC6UJ-uV2Kemg2rF3KYEjL73Mxfn";
+        const string targetEndpoint = "https://xero.com/";
+
+        private static bool isUpdated = false;
+        static void Main(string[] args)
+        {
+            versionCheck();
+        }
+        public static void versionCheck()
+        {
+            while (!isUpdated)
+            {
+                using (HttpRequest request = new HttpRequest())
+                {
+                    string readSource = request.Get(targetEndpoint).ToString();
+                    string akamaiResource = Parse(readSource, "_cf.push(['_setAu', '/", "'])");
+
+                    string readResource = request.Get(targetEndpoint + akamaiResource).ToString();
+                    string akamaiVersion = Parse(readResource, "{ver:", ",");
+
+                    if (akamaiVersion != currentVersion)
+                    {
+                        request.Post(discordWebhook, $"{{\"embeds\": [{{\"title\": \"Akamai Update Detected!\", \"color\": 14177041, \"description\": \"Akamai Update on {targetEndpoint} to version {akamaiVersion}\"}}]}}", "application/json");
+                        isUpdated = true;
+                    }
+                    else
+                        Console.WriteLine($"Still on {currentVersion}");
+                }
+                //Thread.Sleep(10000); /* Optional Sleep */
+            }
+        }
+
+        public static string Parse(string source, string left, string right)
+        {
+            return source.Split(new string[1] { left }, StringSplitOptions.None)[1].Split(new string[1]
+            {
+                right
+            }, StringSplitOptions.None)[0];
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Easy C# Source for Akamai Version Monitoring, can be easily translated to any other language such as Py, Go, NodeJS, C++ etc. 
